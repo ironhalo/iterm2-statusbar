7de3812404e868e6387429b085dc86dad2f0788d
Paul Whitehead <whitehead.pauljay@gmail.com>
2019-11-30 18:06:35 -0700

    add repo config files

---
+++ install.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -e
+
+iterm_scripts_dir="$HOME/Library/Application Support/iTerm2/Scripts"
+autolaunch_dir="$iterm_scripts_dir/AutoLaunch"
+
+mkdir -p "$autolaunch_dir"
+
+echo Linking scripts to AutoLaunch folder "$autolaunch_dir"
+
+for script in *.py
+do
+    echo Copying "$script"...
+    cp "$script" "$autolaunch_dir/$script"
+done
