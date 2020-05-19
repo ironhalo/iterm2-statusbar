ac7720dd8119121c838a077ffdcd656b2d7a367b
Paul Whitehead <whitehead.pauljay@gmail.com>
2020-05-19 10:25:19 -0600

    fix terraform component

--- terraform.py
+++ terraform.py
@@ -4,34 +4,41 @@ Add the following to iterm2_print_user_vars
 iterm2_set_user_var pwd $(pwd)
 """
 
-import asyncio
 import os
 import iterm2
+import subprocess
 
 
 async def main(connection):
     component = iterm2.StatusBarComponent(
-        short_description='terraform workspace',
-        detailed_description='The current Terraform workspace',
-        exemplar='terraform workspace',
+        short_description="terraform workspace",
+        detailed_description="The current Terraform workspace",
+        exemplar="terraform workspace",
         update_cadence=2,
-        identifier='com.ironhalo.iterm-statusbar.tf-workspace',
+        identifier="com.ironhalo.iterm-statusbar.tf-workspace",
         knobs=[],
     )
 
     @iterm2.StatusBarRPC
-    async def tf_workspace(knobs, cwd=iterm2.Reference('user.pwd?')):
-        if os.path.exists(os.path.join(cwd, '.terraform')):
-            proc = await asyncio.create_subprocess_shell(
-                'terraform workspace show',
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE,
+    async def tf_workspace(knobs, cwd=iterm2.Reference("user.pwd?")):
+        path = os.path.join(cwd, ".terraform")
+        if os.path.exists(path):
+            cmd = "terraform workspace show"
+            path = "/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:"
+            proc = subprocess.run(
+                cmd.split(),
+                capture_output=True,
+                check=True,
+                text=True,
+                cwd=cwd,
+                env=dict(PATH=path),
             )
-            stdout, stderr = await proc.communicate()
-            return f'tf: {stdout.decode().strip()}'
+
+            return f"tf: {proc.stdout.strip()}"
         else:
             return ""
 
     await component.async_register(connection, tf_workspace)
+
 
 iterm2.run_forever(main)
